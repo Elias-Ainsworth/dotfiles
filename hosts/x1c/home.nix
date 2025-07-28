6576649435108c27757a14298a278fb50f73d0dd
Elias Ainsworth <pilum-murialis.toge@proton.me>
2025-07-28 02:09:06 -0700

    feat: Change x1c display scale to 1.5...I'm blind.

--- hosts/x1c/home.nix
+++ hosts/x1c/home.nix
@@ -14,7 +14,7 @@ in
         name = "eDP-1";
         width = 1920;
         height = 1080;
-        # scale = 1.2;
+        scale = 1.5;
         refreshRate = if config.specialisation == "otg" then 120 else 60;
         workspaces = [
           1
@@ -57,11 +57,6 @@ in
       enable = true;
       hidden = false;
       # waybar.persistentWorkspaces = true;
-    };
-
-    terminal = {
-      # opacity = "1.0";
-      size = 12;
     };
 
     # don't blind me on startup
